374b8d4793583576b3150116ade76b820fe7dd6e
newtmex <brightomex@gmail.com>
2018-10-22 08:50:14 +0100

    Added first route

--- server.js
+++ server.js
@@ -9,4 +9,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// Routes
+app.all('/', (req, res)=>{
+  res.send('We are ready!');
+})
+
 app.listen(PORT, () => console.log(`server running at port: ${PORT}`))
