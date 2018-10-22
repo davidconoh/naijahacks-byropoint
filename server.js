93cef6e0e506c860d90ea0cf8b1735a14edf8bb4
newtmex <brightomex@gmail.com>
2018-10-22 09:57:08 +0100

    Changed directory structure and added route to index.html

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path')
 
 const app = express();
 const PORT = 8080;
@@ -9,9 +10,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Routes
-app.all('/', (req, res)=>{
-  res.send('We are ready!');
+app.get('/', (req, res)=>{
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
 app.listen(PORT, () => console.log(`server running at port: ${PORT}`))
